7cc6fc917e958cb734b53bc6834aa987e4f26b26
akkinenivijay <akkineni.vijay@gmail.com>
2011-03-13 19:32:03 -0400

    Changed unit testing files.
    Signed-off-by: akkinenivijay <akkineni.vijay@gmail.com>

--- src/test/java/com/cookbook/dao/RecipeDaoImplTest.java
+++ src/test/java/com/cookbook/dao/RecipeDaoImplTest.java
@@ -7,9 +7,10 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cookbook.domain.Author;
@@ -35,16 +36,8 @@ public class RecipeDaoImplTest extends BaseTest {
 	@Autowired
 	private AuthorDao authorDao;
 
-	@BeforeClass
-	public static void initialize() {
-
-	}
-
-	@Test
-	@Transactional
-	// @Rollback(false)
-	public void testSaveRecipe() {
-		// schGen.generateSchema();
+	@Before
+	public void createRecipe() {
 		Recipe recipe = new Recipe();
 		recipe.setContents("This is my contents");
 		recipe.setTitle("Hello Recipe");
@@ -55,22 +48,40 @@ public class RecipeDaoImplTest extends BaseTest {
 		author.setName("Vijay Akkineni");
 
 		recipe.addAuthorToRecipe(author);
+	}
 
-		// recipeDao.save(recipe);
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testSaveRecipe() {
+
 		Assert.assertEquals(1, recipeDao.getAll().size());
 		Assert.assertEquals(1, authorDao.getAll().size());
-
-		logger.debug(recipe);
 
 	}
 
 	@Test
+	@Rollback(true)
+	@Transactional
+	public void testDeleteRecipe() {
+
+		Recipe recipe = recipeDao.get(2L);
+		recipeDao.delete(recipe);
+
+		Recipe recipe2 = recipeDao.get(2L);
+		Assert.assertEquals(null, recipe2);
+	}
+
+	@Test
+	@Rollback(true)
+	@Transactional
 	public void testGetRecipes() {
+
 		List<Recipe> results = recipeDao.getAll();
 		Assert.assertEquals(1, results.size());
 
-		Recipe recipe = recipeDao.get(1L);
-		Assert.assertEquals(1, recipe.getId());
+		Recipe recipe = recipeDao.get(3L);
+		Assert.assertEquals(3, recipe.getId());
 	}
 
 }
